e83d891aad152793c4a2bcbc54b6efe89df44359
POZ <antoivans@gmail.com>
2022-08-01 23:22:05 +0300

    Feat: added list filtering

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
 
 import { TodoItem } from 'components/TodoItem';
+import { selectFilteredTodoIds } from 'store/selectors/todoSelectors';
 import { StyledUl } from './styled';
 
-export const TodoList = ({ todoIds }) => (
-  <StyledUl>
-    {todoIds.map((id) => (
-      <TodoItem key={id} id={id} />
-    ))}
-  </StyledUl>
-);
+export const TodoList = () => {
+  const todoIds = useSelector((state) => {
+    console.log(state);
+    return selectFilteredTodoIds(state);
+  }, shallowEqual);
+  return (
+    <StyledUl>
+      {todoIds.map((id) => (
+        <TodoItem key={id} id={id} />
+      ))}
+    </StyledUl>
+  );
+};
--- src/store/selectors/todoSelectors.js
+++ src/store/selectors/todoSelectors.js
@@ -1,3 +1,5 @@
+import { StatusFilters } from '../reducers/filtersSlice';
+
 export const selectTodoIds = (state) => state.todos.map((todo) => todo.id);
 
 export const selectTodoById = (state, id) =>
@@ -6,4 +8,36 @@ export const selectTodoById = (state, id) =>
 export const selectRemainingCount = (state) => {
   const remaining = state.todos.filter((todo) => !todo.completed);
   return remaining.length;
+};
+
+export const selectFilteredTodoIds = (state) => {
+  const selectedColors = state.filters.colors
+    .filter((elem) => elem.isChecked)
+    .map((elem) => elem.color);
+  return state.todos
+    .filter((elem) => {
+      switch (state.filters.status) {
+        case StatusFilters.all: {
+          console.log(elem.color);
+          console.log(selectedColors);
+          return (
+            selectedColors.indexOf(elem.color) >= 0 ||
+            (elem.color === 'none' && !selectedColors.length)
+          );
+        }
+        case StatusFilters.active: {
+          return (
+            (selectedColors.indexOf(elem.color) >= 0 && !elem.completed) ||
+            (elem.color === 'none' && !selectedColors.length && !elem.completed)
+          );
+        }
+        case StatusFilters.completed: {
+          return (
+            (selectedColors.indexOf(elem.color) >= 0 && elem.completed) ||
+            (elem.color === 'none' && !selectedColors.length && elem.completed)
+          );
+        }
+      }
+    })
+    .map((elem) => elem.id);
 };
